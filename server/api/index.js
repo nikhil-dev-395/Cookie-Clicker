7555c1ffa42febf0fc33e8e96553e202e0a10019
nikhil-dev-395 <nikhilw395@gmail.com>
2025-01-27 01:47:10 +0530

    cors added

--- server/api/index.js
+++ server/api/index.js
@@ -11,6 +11,7 @@ const authUser = require("../src/middlewares/auth.middleware.js");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 app.use(morgan("dev"));
 
 /*ROUTES*/
